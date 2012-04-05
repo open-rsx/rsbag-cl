03f0dc81df21cad11cd8e52680567af5e00bdd07
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-04-05 11:20:37 +0000

    Support 0.6 version in src/transform/rsb-event-multi-version.lisp * src/transform/rsb-event-multi-version.lisp   (define-serialization-version): added versioned? keyword parameter   to support versioned serialization implementations   (define-serialization-version 0.6): load and register 0.6 version of   serialization * compat/0.6: new directory; contains 0.6 version of event   serialization

---
+++ compat/0.6/src/transform/package.lisp
@@ -0,0 +1,64 @@
+;;; package.lisp --- Package definition for the transform module.
+;;
+;; Copyright (C) 2011, 2012 Jan Moringen
+;;
+;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
+;;
+;; This Program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This Program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses>.
+
+(cl:defpackage :rsbag.transform
+  (:use
+   :cl
+   :alexandria
+   :bind
+   :iterate)
+
+  (:import-from :rsb
+   :chainable-condition
+   :chainable-condition-cause)
+
+  ;; Variables
+  (:export
+   :+rsb-schema-name+)
+
+  ;; Conditions
+  (:export
+   :transform-error
+   :transform-error-transform
+
+   :encoding-error
+   :transform-error-domain-object
+
+   :decoding-error
+   :transform-error-encoded)
+
+  ;; Transform protocol
+  (:export
+   :transform-name
+   :transform-format
+
+   :decode
+   :encode)
+
+  ;; Findable transform class family
+  (:export
+   :no-such-transform-class
+   :find-transform-class
+   :transform-classes
+
+   :make-transform)
+
+  (:documentation
+   "This package contains the transformation protocol and
+infrastructure used in cl-rsbag."))
--- src/transform/rsb-event-multi-version.lisp
+++ src/transform/rsb-event-multi-version.lisp
@@ -19,7 +19,7 @@
 
 (cl:in-package :rsbag.transform)
 
-(defmacro define-serialization-version (version)
+(defmacro define-serialization-version (version &key versioned?)
   "Define a serialization version VERSION."
   (bind (((:flet versioned-symbol (name package))
 	  (find-symbol name (find-package (make-versioned-name
@@ -34,7 +34,9 @@
 	"Handle ARGS appropriately."
 	(make-instance (,(versioned-symbol
 			  "FIND-TRANSFORM-CLASS" :rsbag.transform)
-			 :rsb-event)
+			 ,(if versioned?
+			      (format-symbol :keyword "RSB-EVENT-~A" version)
+			      :rsb-event))
 		       :wire-schema (first args))
 
 	(bind (((wire-schema &rest rest) args)
@@ -60,6 +62,39 @@
 			 (domain-object t))
 	(,(versioned-symbol "ENCODE" :rsbag.transform)
 	  transform domain-object)))))
+
+
+;;; 0.6 Version
+;;
+
+(eval-when (:compile-toplevel :load-toplevel)
+  (with-versioned-packages ("0.6"
+			    :rsb.protocol
+			    :rsbag.transform)
+    (with-compilation-unit ()
+      (let* ((path                  (asdf:system-relative-pathname
+				     :cl-rsbag "compat/0.6/"))
+	     (pbf:*proto-load-path* (cons (merge-pathnames "data/" path)
+					  pbf:*proto-load-path*)))
+	;; Load relevant protocol buffer types.
+	(iter (for file in '("data/rsb/protocol/EventId.proto"
+			     "data/rsb/protocol/EventMetaData.proto"
+			     "data/rsb/protocol/Notification.proto"))
+	      (map nil (curry #'pbb:emit (pbf:load/text (merge-pathnames file path)))
+		   '(:class :packed-size :serializer :deserializer)))
+	;; Load implementation.
+	(map nil (compose #'load (rcurry #'merge-pathnames path))
+	     '("src/transform/package.lisp"
+	       "src/transform/protocol.lisp"
+	       "src/transform/conditions.lisp"
+	       "src/transform/rsb-event.lisp"))
+	;; Inject the version of rsb-event that supports payload
+	;; conversion.
+	(load (asdf:component-pathname
+	       (asdf:find-component
+		:cl-rsbag '("rsb-serialization" "rsb-event-payload-conversion"))))))))
+
+(define-serialization-version "0.6" :versioned? t)
 
 
 ;;; 0.5 Version
