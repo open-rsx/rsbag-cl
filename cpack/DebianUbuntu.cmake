749e13a3bd0f53a3eb7c8cfc2b2041b4879d6807
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-09-19 09:16:09 +0700

    Fixed Debian compliance issues in cpack/CPackDebianConfig.cmake
    * cpack/CPackDebianConfig.cmake: install copyright file; write and
      install upstream and Debian changelog by transforming the git log

--- cpack/DebianUbuntu.cmake
+++ cpack/DebianUbuntu.cmake
@@ -46,6 +46,35 @@ FILE(WRITE "${PRERM_SCRIPT}"
 EXECUTE_PROCESS(COMMAND chmod 755 "${POSTINST_SCRIPT}" "${PRERM_SCRIPT}")
 SET(CPACK_DEBIAN_PACKAGE_CONTROL_EXTRA "${POSTINST_SCRIPT};${PRERM_SCRIPT}")
 
+# Generate required change log files.
+FIND_PROGRAM(LSB_EXECUTABLE "lsb_release")
+EXECUTE_PROCESS(COMMAND ${LSB_EXECUTABLE} --short --codename
+                OUTPUT_VARIABLE LSB_CODENAME
+                OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+                        log "--format=%ad  %an  <%ae>%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n"
+                        --date=short
+                COMMAND gzip -9
+                OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.gz")
+EXECUTE_PROCESS(COMMAND sh -c "echo -n \"sed -e '\" ; for c in $(${GIT_EXECUTABLE} rev-list --all -- \"${CMAKE_CURRENT_LIST_FILE}\") ; do echo -n \"s/$c/$(${GIT_EXECUTABLE} describe --tags $c | sed -re s/[^0-9]*\\([0-9]+\\)\\.\\([0-9]+\\)-\\([0-9]+\\)-.*/\\\\1.\\'\\$\\(\\(\\\\2+1\\)\\)\\'.\\\\3/)/\\;\" ; done ; echo \"'\""
+                OUTPUT_VARIABLE COMMIT_TO_VERSION_SED_RULES)
+EXECUTE_PROCESS(COMMAND ${GIT_EXECUTABLE}
+                        log "--format=${CPACK_DEBIAN_PACKAGE_NAME} (%H) ${LSB_CODENAME}; urgency=low%n%n%w(76,8,10)%s%w(76,8,8)%n%n%b%n%n%w(200,1,1)-- %an <%ae>  %ad%n"
+                        --date=rfc
+                        -- "${CMAKE_CURRENT_LIST_FILE}"
+                COMMAND sh -c ${COMMIT_TO_VERSION_SED_RULES}
+                COMMAND gzip -9
+                OUTPUT_FILE "${CMAKE_BINARY_DIR}/changelog.Debian.gz")
+INSTALL(FILES "${CMAKE_BINARY_DIR}/changelog.gz"
+              "${CMAKE_BINARY_DIR}/changelog.Debian.gz"
+        DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}")
+
+# Install copyright file.
+INSTALL(FILES "${CMAKE_SOURCE_DIR}/COPYING"
+        DESTINATION "share/doc/${CPACK_DEBIAN_PACKAGE_NAME}"
+        RENAME      copyright)
+
 SET(CPACK_PACKAGE_FILE_NAME "${CPACK_DEBIAN_PACKAGE_NAME}-${CPACK_DEBIAN_PACKAGE_VERSION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}")
 
 MESSAGE(STATUS "Debian Package: ${CPACK_DEBIAN_PACKAGE_NAME} (${CPACK_DEBIAN_PACKAGE_VERSION}) [${CPACK_PACKAGE_FILE_NAME}.deb]")
