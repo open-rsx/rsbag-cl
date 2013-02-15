3ea76711162aec6116f33808749419b46f1b6d88
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-02-15 22:16:21 +0100

    Improved package documentation in src/backend/tidelog/package.lisp
    * src/backend/tidelog/package.lisp (header): updated copyright
      (package rsbag.backend.tidelog): reformatted and extended
      documentation string

--- src/backend/tidelog/package.lisp
+++ src/backend/tidelog/package.lisp
@@ -1,6 +1,6 @@
 ;;; package.lisp --- Package definition for the backend.tidelog module.
 ;;
-;; Copyright (C) 2011, 2012 Jan Moringen
+;; Copyright (C) 2011, 2012, 2013 Jan Moringen
 ;;
 ;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 ;;
@@ -47,21 +47,52 @@
 format as specified at https://retf.info/svn/drafts/rd-0001.txt.
 
 The most important interface classes are:
-+ `file' which represents an entire TIDE log file (and handles
-  access to its channels)
-+ `index' which represents the index for one channel of a TIDE log
-  file
+
+file                                [class]
+
+  Represents an entire TIDE log file (and handles access to its
+  channels).
+
+index                               [class]
+
+  Represents the index for one channel of a TIDE log file.
 
 In addition, there is lower-level input/output machinery based on code
 generated according to the TIDE log specification. This level operates
 on TIDE log blocks.
 
 The interface consists of the generic functions
-+ `scan' which finds blocks in a TIDE log file without reading their
-  entire contents
-+ `pack' which stores blocks in TIDE log files
-+ `unpack' which reads blocks from TIDE log files
+
+tag                                 [generic function]
+
+  Returns the four-character \"tag-string\" identifying the block type
+  of an object or class.
+
+size                                [generic function]
+
+  Returns the number of octets required to store a block object.
+
+scan                                [generic function]
+
+  Finds blocks in a TIDE log file without reading their entire
+  contents.
+
+unpack                              [generic function]
+
+   Which reads blocks from TIDE log files.
+
+pack                                [generic function]
+
+   Which stores blocks in TIDE log files
 
 The code generation is implemented by
-+ spec->* family of functions
-+ `define-element' macro."))
+
+spec->*                             [function]
+
+  Family of functions for generating methods on the above generic
+  functions for specified block types.
+
+define-element                      [macro]
+
+  Turns declarative block type specifications into calls to spec->*
+  functions."))
