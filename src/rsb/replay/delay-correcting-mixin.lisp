5cba4165958e41fdbb2f75b3ba12b41647ede348
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-03-05 02:25:16 +0100

    Fixed type in src/rsb/replay/delay-correcting-mixin.lisp
    * src/rsb/replay/delay-correcting-mixin.lisp (header): updated
      copyright
      (delay-correcting-mixin::previous-delay): allow negative values;
      adjusted documentation string accordingly
      (schedule-event :around delay-correcting-mixin t timestamp timestamp):
      explain how negative delays can happen

--- src/rsb/replay/delay-correcting-mixin.lisp
+++ src/rsb/replay/delay-correcting-mixin.lisp
@@ -1,6 +1,6 @@
 ;;; delay-correcting-mixin.lisp --- A mixin class for correcting delays.
 ;;
-;; Copyright (C) 2011, 2012 Jan Moringen
+;; Copyright (C) 2011, 2012, 2013 Jan Moringen
 ;;
 ;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 ;;
@@ -25,12 +25,13 @@
 (cl:in-package :rsbag.rsb.replay)
 
 (defclass delay-correcting-mixin ()
-  ((previous-delay :type     (or null non-negative-real)
+  ((previous-delay :type     (or null real)
 		   :accessor strategy-previous-delay
 		   :initform nil
 		   :documentation
 		   "Stores the previously scheduled delay to estimate
-the difference between the scheduled and actual delay.")
+the difference between the scheduled and actual delay. This can become
+negative when the previous wait executed too slowly.")
    (previous-call  :type     (or null local-time:timestamp)
 		   :accessor strategy-previous-call
 		   :initform nil
@@ -50,16 +51,24 @@ this delay adjusted to compensate for processing latencies."))
   ;; Compute the difference between the previously scheduled duration
   ;; and the actual duration. Adjust the next scheduled duration
   ;; accordingly.
+  ;;
+  ;; CORRECTED can become negative if at the same time
+  ;; 1) the planned delay (returned by (call-next-method) is very
+  ;;    small
+  ;; 2) OVERSHOOT is positive
+  ;; When this happens, no wait is performed and the negative
+  ;; PREVIOUS-DELAY hopefully leads to a compensation in the next
+  ;; call.
   (let+ (((&accessors (previous-delay strategy-previous-delay)
 		      (previous-call  strategy-previous-call)) strategy)
 	 (now          (local-time:now))
 	 (actual-delay (when previous-call
 			 (local-time:timestamp-difference
 			  now previous-call)))
-	 (overshoot    (if (and actual-delay previous-delay )
+	 (overshoot    (if (and actual-delay previous-delay)
 			   (- actual-delay previous-delay)
 			   0))
 	 (corrected    (- (call-next-method) overshoot)))
     (setf previous-delay corrected
-	  previous-call     now)
+	  previous-call  now)
     corrected))
