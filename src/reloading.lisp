29f1b2a2b8621b80ab424e99617f2e0e61bef5cd
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-01-02 14:03:52 +0100

    Use uiop:register-image-{dump,restore}-hook in src/reloading.lisp
    * src/reloading.lisp (header): updated copyright
      (enable-restart-threadpool): removed
      (toplevel): register `{stop,start}-threadpool' as image {dump,restore}
      hook respectively

--- src/reloading.lisp
+++ src/reloading.lisp
@@ -1,15 +1,13 @@
 ;;;; reloading.lisp --- Support for image dumping and reloading.
 ;;;;
-;;;; Copyright (C) 2013 Jan Moringen
+;;;; Copyright (C) 2013, 2015 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:rsbag)
 
-(defun enable-restart-threadpool ()
-  "Make sure that the rsbag threadpool is shutdown when saving a core
-   and restarted when loading a core."
-  #+sbcl (progn
-           (pushnew 'stop-threadpool  sb-ext:*save-hooks*)
-           (pushnew 'start-threadpool sb-ext:*init-hooks*))
-  #-sbcl (error "Restarting the rsbag threadpool is not supported in this Lisp."))
+;; Make sure that the rsbag threadpool is shutdown when dumping an
+;; image and restarted when restoring an image.
+
+(uiop:register-image-dump-hook 'stop-threadpool nil)
+(uiop:register-image-restore-hook 'start-threadpool nil)
