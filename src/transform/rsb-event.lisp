3403c984cb37f4ba5b308fde39f27112129767c2
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-11-11 19:08:26 +0100

    Cleanup in src/transform/rsb-event.lisp
    * src/transform/rsb-event.lisp (keyword->bytes): fixed ftype declamation
      (bytes->keyword): likewise
      (%reset-holder): new function; reset notification instance
      (decode rsb-event simple-array): use `%reset-holder'; simplifications

--- src/transform/rsb-event.lisp
+++ src/transform/rsb-event.lisp
@@ -40,7 +40,8 @@
   "Convert BYTES into a string."
   (sb-ext:octets-to-string bytes :external-format :ascii))
 
-(declaim (ftype (function (keyword) simple-octet-vector) keyword->bytes))
+(declaim (ftype (function (keyword) (values simple-octet-vector &optional))
+                keyword->bytes))
 
 (defun keyword->bytes (keyword)
   "Convert the name of KEYWORD into a `simple-octet-vector'."
@@ -49,15 +50,27 @@
       (let ((*readtable* *keyword-readtable*))
         (string->bytes (princ-to-string keyword)))))
 
-(declaim (ftype (function (simple-octet-vector) keyword) bytes->keyword))
+(declaim (ftype (function (simple-octet-vector) (values keyword &optional))
+                bytes->keyword))
 
 (defun bytes->keyword (bytes)
   "Converter BYTES into a keyword."
   (if (find (char-code #\:) bytes)
-      (intern (bytes->string bytes) #.(find-package '#:keyword))
+      (values (intern (bytes->string bytes) #.(find-package '#:keyword)))
       (let ((*package*   #.(find-package '#:keyword))
             (*readtable* *keyword-readtable*))
-        (read-from-string (bytes->string bytes)))))
+        (values (read-from-string (bytes->string bytes))))))
+
+(declaim (inline %reset-holder))
+
+(defun %reset-holder (holder)
+  (let+ (((&accessors-r/o (meta-data rsb.protocol:notification-meta-data)
+                          (causes    rsb.protocol:notification-causes))
+          holder))
+    (setf (fill-pointer (rsb.protocol:event-meta-data-user-infos meta-data)) 0
+          (fill-pointer (rsb.protocol:event-meta-data-user-times meta-data)) 0
+          (fill-pointer causes)                                              0))
+  holder)
 
 ;;; Transform
 
@@ -191,23 +204,19 @@
     (pb:pack* holder)))
 
 (defmethod decode ((transform rsb-event) (data simple-array))
-  (let+ (((&flet decode-event-id (id)
-            (cons (uuid:byte-array-to-uuid
-                   (rsb.protocol:event-id-sender-id id))
-                  (rsb.protocol:event-id-sequence-number id))))
-         ((&accessors-r/o (holder transform-%holder)) transform)
+  (let+ (((&accessors-r/o (holder transform-%holder)) transform)
          ((&accessors-r/o (id        rsb.protocol:notification-event-id)
                           (meta-data rsb.protocol:notification-meta-data)
                           (causes    rsb.protocol:notification-causes))
-          holder)
-         ;; Create output event.
-         (event
-          (progn
-            (setf (fill-pointer (rsb.protocol:event-meta-data-user-infos meta-data)) 0
-                  (fill-pointer (rsb.protocol:event-meta-data-user-times meta-data)) 0
-                  (fill-pointer causes)                                              0)
-            (pb:unpack data holder)
-
+          holder))
+    ;; Unpack raw data into HOLDER notification instance.
+    (pb:unpack data (%reset-holder holder))
+    ;; Construct result event.
+    (let+ (((&flet decode-event-id (id)
+              (cons (uuid:byte-array-to-uuid
+                     (rsb.protocol:event-id-sender-id id))
+                    (rsb.protocol:event-id-sequence-number id))))
+           (event
             (make-instance
              'rsb:event
              :sequence-number   (rsb.protocol:event-id-sequence-number id)
@@ -219,35 +228,33 @@
                                   (bytes->keyword
                                    (rsb.protocol:notification-method holder)))
              :data              (rsb.protocol:notification-data holder)
-             :causes            (map 'list #'decode-event-id
-                                     (rsb.protocol:notification-causes holder))
+             :causes            (map 'list #'decode-event-id causes)
              :create-timestamp? nil
-             :intern-scope?     t)))
-         ((&flet process-timestamp (name value)
-            (unless (zerop value)
-              (setf (rsb:timestamp event name)
-                    (unix-microseconds->timestamp value))))))
+             :intern-scope?     t))
+           ((&flet process-timestamp (name value)
+              (unless (zerop value)
+                (setf (rsb:timestamp event name)
+                      (unix-microseconds->timestamp value))))))
+      ;; Fill fixed timestamps.
+      (process-timestamp :create  (rsb.protocol:event-meta-data-create-time  meta-data))
+      (process-timestamp :send    (rsb.protocol:event-meta-data-send-time    meta-data))
+      (process-timestamp :receive (rsb.protocol:event-meta-data-receive-time meta-data))
+      (process-timestamp :deliver (rsb.protocol:event-meta-data-deliver-time meta-data))
 
-    ;; Fill fixed timestamps.
-    (process-timestamp :create  (rsb.protocol:event-meta-data-create-time  meta-data))
-    (process-timestamp :send    (rsb.protocol:event-meta-data-send-time    meta-data))
-    (process-timestamp :receive (rsb.protocol:event-meta-data-receive-time meta-data))
-    (process-timestamp :deliver (rsb.protocol:event-meta-data-deliver-time meta-data))
+      ;; Add user meta-data.
+      (iter (for item each (rsb.protocol:event-meta-data-user-infos meta-data))
+            (setf (rsb:meta-data
+                   event (bytes->keyword (rsb.protocol:user-info-key item)))
+                  (bytes->string (rsb.protocol:user-info-value item))))
 
-    ;; Add user meta-data.
-    (iter (for item each (rsb.protocol:event-meta-data-user-infos meta-data))
-          (setf (rsb:meta-data
-                 event (bytes->keyword (rsb.protocol:user-info-key item)))
-                (bytes->string (rsb.protocol:user-info-value item))))
+      ;; Add user timestamps.
+      (iter (for time each (rsb.protocol:event-meta-data-user-times meta-data))
+            (setf (rsb:timestamp
+                   event (bytes->keyword (rsb.protocol:user-time-key time)))
+                  (unix-microseconds->timestamp
+                   (rsb.protocol:user-time-timestamp time))))
 
-    ;; Add user timestamps.
-    (iter (for time each (rsb.protocol:event-meta-data-user-times meta-data))
-          (setf (rsb:timestamp
-                 event (bytes->keyword (rsb.protocol:user-time-key time)))
-                (unix-microseconds->timestamp
-                 (rsb.protocol:user-time-timestamp time))))
-
-    event))
+      event)))
 
 (defmethod print-object ((object rsb-event) stream)
   (print-unreadable-object (object stream :type t :identity t)
